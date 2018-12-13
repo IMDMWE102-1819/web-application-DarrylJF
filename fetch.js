0f121c9136360a4d7884682aa127b3e0b5d780cc
darryljf <darryl.jade.frost@gmail.com>
2018-12-13 11:51:57 +0000

    first commit

---
+++ fetch.js
@@ -0,0 +1,17 @@
+
+// calling fetch returns a promise
+// promises is defined as a proxy for a value that will eventually become available
+// promises are one way to deal with asynchronous code, without writing too many callbacks in your code
+
+/*fetch("./test.json")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+    })*/
+
+    
+
+
+
